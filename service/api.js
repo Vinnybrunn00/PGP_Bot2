7b6ee28f8faa0df5df0f16b62e58219e359bd6fc
Vinicius Bruno <vinibruno.dev@gmail.com>
2024-07-01 12:17:12 -0300

    PGP2

--- service/api.js
+++ service/api.js
@@ -5,10 +5,11 @@ class Api {
     }
 
     async formatTime() {
+        
         let getMonth = this.data.getMonth() + 1
         let dataFormats = `${this.data.getDate() < 10 ? '0' + this.data.getDate() : this.data.getDate()}/${this.data.getMonth() < 10 ? '0' + getMonth : getMonth}/${this.data.getFullYear()}`
         let hourFormats = `${this.data.getHours() < 10 ? '0' + this.data.getHours() : this.data.getHours()}:${this.data.getMinutes() < 10 ? '0' + this.data.getMinutes() : this.data.getMinutes()}:${this.data.getSeconds() < 10 ? '0' + this.data.getSeconds() : this.data.getSeconds()}`
-        return dataFormats + ' ás ' + hourFormats
+        return dataFormats + ' ás aaaa' + hourFormats
     }
 
     async formatTimeStamp(date) {
